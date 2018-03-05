352809d72755f718bc9383e4163c7dc2bb93a614
zhouguang <zhouguang@kenrobot.com>
2018-03-05 16:18:16 +0800

    update scrath 3.0

--- src/renderer/assets/js/app/index/config/menu.js
+++ src/renderer/assets/js/app/index/config/menu.js
@@ -23,17 +23,11 @@ define(function() {
 	}, {
 		placeholder: "帮助",
 		menu: [{
-			text: "Arduino驱动下载",
-			action: "download-arduino-driver",
-		}, "_", {
 			text: "检查更新",
 			action: "check-update",
 		}, {
 			text: "啃萝卜官网",
 			action: "visit-kenrobot",
-		}, {
-			text: "Arduino论坛",
-			action: "visit-arduino",
 		}, "_", {
 			text: "建议反馈",
 			action: "suggestion",
